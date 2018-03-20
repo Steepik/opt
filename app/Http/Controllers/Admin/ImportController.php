753c08cfa4ae6ea10ef66e0cc63edabfd438acb4
Steepik <dimachka2009@yandex.ru>
2018-03-20 12:40:47 +0300

    Available make price list for autopiter

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\ImportExcelRequest;
 use App\ImportExcelToDb;
 use App\Http\Controllers\Controller;
+use Excel;
+use App\Tire;
+use App\Wheel;
+use Illuminate\Support\Collection;
 
 class ImportController extends Controller
 {
@@ -21,5 +25,47 @@ class ImportController extends Controller
 
             return redirect()->back()->with('updated', '');
         }
+    }
+
+    public function autopiter()
+    {
+            Excel::create('Price-list', function($excel){
+                $excel->sheet('Price-list', function($sheet){
+                    $sheet->setAutoSize(true);
+                    $sheet->rows(array(
+                        array(
+                            'Каталог',
+                            'Номер',
+                            'Наименование',
+                            'Цена',
+                            'Наличие',
+                        ),
+                    ));
+                    $tires = Tire::where('quantity', '>', 0)->get();
+                    $wheels = Wheel::where('quantity', '>', 0)->get();
+                    $tires->each(function($item){
+                        $item['brand'] = $item->brand->name;
+                    });
+                    $wheels->each(function($item){
+                        $item['brand'] = $item->brand->name;
+                    });
+
+                    $data = array_merge($tires->toArray(), $wheels->toArray());
+
+                    foreach($data as $item) {
+                        $sheet->rows(array(
+                            array(
+                                $item['brand']['name'],
+                                $item['tcae'],
+                                $item['name'],
+                                $item['price_opt'],
+                                $item['quantity'],
+                            ),
+                        ));
+                    }
+
+
+                });
+            })->download('xls');
     }
 }
